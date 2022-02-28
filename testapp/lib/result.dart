5f758e8282c0b0044ac4507bb0ee8d9b725a1186
jrajawat <jinal@rogers.com>
2022-02-27 21:36:14 -0500

    blah balha

---
+++ testapp/lib/result.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+//Remark Logic
+  String get resultPhrase {
+    String resultText;
+    if (resultScore >= 41) {
+      resultText = 'You are awesome!';
+      print(resultScore);
+    } else if (resultScore >= 31) {
+      resultText = 'Pretty likeable!';
+      print(resultScore);
+    } else if (resultScore >= 21) {
+      resultText = 'You need to work more!';
+    } else if (resultScore >= 1) {
+      resultText = 'You need to work hard!';
+    } else {
+      resultText = 'This is a poor score!';
+      print(resultScore);
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ), //Text
+          Text(
+            'Score ' '$resultScore',
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ), //Text
+          FlatButton(
+            child: Text(
+              'Restart Quiz!',
+            ), //Text
+            textColor: Colors.blue,
+            onPressed: resetHandler,
+          ), //FlatButton
+        ], //<Widget>[]
+      ), //Column
+    ); //Center
+  }
+}
